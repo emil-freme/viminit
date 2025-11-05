244756d85e3b100f741a3a5a8e0d45395f341832
Emil Freme <emil@1000fps.co>
2025-11-05 20:45:32 -0300

    Better mappings

--- lua/emil/keymaps.lua
+++ lua/emil/keymaps.lua
@@ -1,5 +1,6 @@
-
+-- Autosave
 vim.keymap.set('i', '<ESC>', '<ESC> :w<CR>')
+
 -- LSP 
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
@@ -16,13 +17,38 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
     vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = event.buf, desc = 'Go to [P]revious [D]iagnostic' })
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = event.buf, desc = 'Go to [N]ext [D]iagnostic' })
   end,
 })
 
+-- Focus
 vim.api.nvim_set_keymap("n", "<leader>zn", ":Focus<CR>", {})
 vim.api.nvim_set_keymap("v", "<leader>zn", ":'<,'>Narrow<CR>:Focus<CR>", {})
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+
+-- Themes
 vim.api.nvim_set_keymap("n", "<leader>t1", ":colorscheme pastel-peach<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>t2", ":colorscheme pastel-peach-dark<CR>", {})
+
+-- Neogit
+vim.keymap.set('n', '<leader>gg', ':Neogit<CR>', { desc = '[G]it Status' })
+
+-- Tagbar
+vim.keymap.set('n', '<leader>tb', ':TagbarToggle<CR>', { desc = '[T]oggle [B]ar' })
+
+-- img-clip (Paste Image)
+vim.keymap.set('n', '<leader>pi', ':PasteImage<CR>', { desc = '[P]aste [I]mage' })
+
+-- Markview
+vim.keymap.set('n', '<leader>mp', ':Markview<CR>', { desc = '[M]arkdown [P]review' })
+
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "rounded",  -- Border style: "single", "double", "rounded", "solid", or "shadow"
